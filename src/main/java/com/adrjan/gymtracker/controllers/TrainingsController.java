8615258061006bf45145487caf79ef07189c6989
Adrian Jankowski <jankowski.adrian13@gmail.com>
2022-10-23 22:41:29 +0200

    - added base page for trainings.

---
+++ src/main/java/com/adrjan/gymtracker/controllers/TrainingsController.java
@@ -0,0 +1,15 @@
+package com.adrjan.gymtracker.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/trainings")
+public class TrainingsController {
+
+    @GetMapping
+    public String showPage() {
+        return "trainings";
+    }
+}
