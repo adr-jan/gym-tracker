d579e454a31a04cbaa8acfdbba1057b355311117
Adrian Jankowski <jankowski.adrian13@gmail.com>
2022-10-23 23:27:45 +0200

    - added trainings base list (acordeon).

--- src/main/java/com/adrjan/gymtracker/controllers/TrainingsController.java
+++ src/main/java/com/adrjan/gymtracker/controllers/TrainingsController.java
@@ -1,6 +1,9 @@
 package com.adrjan.gymtracker.controllers;
 
+import com.adrjan.gymtracker.repositories.TrainingsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,8 +11,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/trainings")
 public class TrainingsController {
 
+    @Autowired
+    TrainingsRepository trainingsRepository;
+
     @GetMapping
-    public String showPage() {
+    public String showPage(Model model) {
+        model.addAttribute("trainings", trainingsRepository.findAll());
         return "trainings";
     }
 }
