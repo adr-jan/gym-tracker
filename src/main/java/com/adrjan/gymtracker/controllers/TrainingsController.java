f93e634805c998797182a64a102fbd8ae986938e
Adrian Jankowski <jankowski.adrian13@gmail.com>
2022-10-24 21:45:50 +0200

    - changed how training is displayed.

--- src/main/java/com/adrjan/gymtracker/controllers/TrainingsController.java
+++ src/main/java/com/adrjan/gymtracker/controllers/TrainingsController.java
@@ -1,11 +1,19 @@
 package com.adrjan.gymtracker.controllers;
 
+import com.adrjan.gymtracker.entity.TrainingSession;
 import com.adrjan.gymtracker.repositories.TrainingsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.text.SimpleDateFormat;
+import java.util.*;
+import java.util.stream.StreamSupport;
 
 @Controller
 @RequestMapping("/trainings")
@@ -16,7 +24,20 @@ public class TrainingsController {
 
     @GetMapping
     public String showPage(Model model) {
-        model.addAttribute("trainings", trainingsRepository.findAll());
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("E     dd-MM-yyyy HH:mm");
+        Map<TrainingSession, String> trainings = new HashMap<>();
+
+        trainingsRepository.findAll().forEach(
+                training -> trainings.put(training, simpleDateFormat.format(training.getCreatedAt()))
+        );
+
+        model.addAttribute("trainings", trainings);
         return "trainings";
+    }
+
+    @PostMapping
+    public String selectTraining(@ModelAttribute TrainingSession selectedTraining, RedirectAttributes redirectAttributes) {
+        redirectAttributes.addFlashAttribute("training", selectedTraining);
+        return "redirect:/trainings";
     }
 }
